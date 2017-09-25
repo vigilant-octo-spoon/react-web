970a822e82d373720208f2d96dea71aade61939a
Rodolfo Palma Otero <rpalmaotero@gmail.com>
2017-09-25 09:43:41 -0300

    Add login container and component

---
+++ src/components/LoginForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class LoginForm extends Component {
+    render() {
+        return (
+            <div>
+                <h1>LoginForm</h1>
+            </div>
+
+        )
+    }
+}
+
+export default LoginForm;
