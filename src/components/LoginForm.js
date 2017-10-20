07b20b8f9aed154c1406e4b7fdfa495e240527e1
ctalvarez <ctalvarez@uc.cl>
2017-10-20 01:26:26 -0300

    login component, containers files

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 
 class LoginForm extends Component {
-    render() {
-        return (
-            <div>
-                <h1>LoginForm</h1>
-            </div>
 
-        )
-    }
+	render() {
+	    return (
+	      	<div>
+		      	<MuiThemeProvider>
+		        	<div>
+		        		<AppBar
+		           		title="Login"
+			           	/>
+			           	<TextField
+		            		 hintText="Enter your Username"
+				             floatingLabelText="Username"
+				        />
+				        <br/>
+				        <TextField
+				  	     	type="password"
+				         	hintText="Enter your Password"
+				            floatingLabelText="Password"
+				        />
+				        <br/>
+				        <RaisedButton label="Submit" onClick=""/>
+		          	</div>
+		  	  	</MuiThemeProvider>
+	    	</div>
+
+	    )
+	}
 }
 
 export default LoginForm;
---
+++ src/containers/LoginContainer.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import LoginForm from '../components/LoginForm.js';
+
+class LoginContainer extends Component {
+    render() {
+        return (
+            <div>
+                <LoginForm />
+            </div>
+
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+
+    }
+}
+
+/*export default connect(mapStateToProps)(LoginPage);*/
+export default LoginContainer
