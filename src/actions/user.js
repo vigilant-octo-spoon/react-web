9b09a33c76e1cd6bbfa7590f9e62799a6df42f4f
Rodolfo Palma Otero <rpalmaotero@gmail.com>
2017-10-20 01:26:49 -0300

    Add basic redux structure

---
+++ src/actions/user.js
@@ -0,0 +1,10 @@
+export const POST_LOGIN = 'user/POST_LOGIN';
+export const POST_LOGIN_SUCCESS = 'user/POST_LOGIN_SUCCESS';
+export const POST_LOGIN_FAILED = 'user/POST_LOGIN_FAILED';
+export const LOGOUT = 'user/LOGOUT';
+
+export const postLogin = (data) => {
+    return (dispatch) => {
+
+    }
+}
---
+++ src/reducers/user.js
@@ -0,0 +1,12 @@
+const intialState = {
+    identifier: null,
+    token: null,
+    isLogged: false,
+};
+
+export default (state = intialState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
