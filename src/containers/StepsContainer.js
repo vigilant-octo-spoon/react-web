a3b046811d28312c4056c11724ba43b08260661a
Rodolfo Palma Otero <rpalmaotero@gmail.com>
2017-11-10 01:57:55 -0300

    Add margin to StepsContainer

--- src/containers/StepsContainer.js
+++ src/containers/StepsContainer.js
@@ -8,41 +8,41 @@ import RecursosView from '../components/RecursosView.js'
 import '../styles/Steps.scss';
 
 class StepsContainer extends Component {
-
-    
     render() {
         return (
-            <Card >
-            <CardTitle title="Methodology title" subtitle="User Name - User email" />
-            <Divider />
-            <CardTitle title="Planificación" />  
-              <PlanificarView title="Nombre de la iniciativa"
-                              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa. Aliquam erat volutpat."
-              />
-              <PlanificarView title="Objetivo"
-                              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa. Aliquam erat volutpat."
-              />
-              <PlanificarView title="Lugar de implementación"
-                              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa. Aliquam erat volutpat."
-              />
-              <PlanificarView title="Inicio - Término"
-                              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa. Aliquam erat volutpat."
-              />
-              <PlanificarView title="Equipo de trabajo"
-                              members={["carlos - jefe de grupo", "rodolfo - junior"]}
-                              id="team-view"
-              />
-              <RecursosView recursos={[{item: 'caca', available:'sí', acquisition: 'ya adquerido'},
-                                      {item: 'pico', available:'no', acquisition: 'se espera que el colegio coopere'}]}
-              />
+            <div id="steps-container">
+                <Card >
+                <CardTitle title="Methodology title" subtitle="User Name - User email" />
+                <Divider />
+                <CardTitle title="Planificación" />  
+                <PlanificarView title="Nombre de la iniciativa"
+                                text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa. Aliquam erat volutpat."
+                />
+                <PlanificarView title="Objetivo"
+                                text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa. Aliquam erat volutpat."
+                />
+                <PlanificarView title="Lugar de implementación"
+                                text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa. Aliquam erat volutpat."
+                />
+                <PlanificarView title="Inicio - Término"
+                                text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa. Aliquam erat volutpat."
+                />
+                <PlanificarView title="Equipo de trabajo"
+                                members={["carlos - jefe de grupo", "rodolfo - junior"]}
+                                id="team-view"
+                />
+                <RecursosView recursos={[{item: 'caca', available:'sí', acquisition: 'ya adquerido'},
+                                        {item: 'pico', available:'no', acquisition: 'se espera que el colegio coopere'}]}
+                />
+                    
                 
-              
-                
-                <CardActions>
-                  <FlatButton label="Action 1" />
-                  <FlatButton label="Action 2" />
-                </CardActions>
-            </Card>
+                    
+                    <CardActions>
+                    <FlatButton label="Action 1" />
+                    <FlatButton label="Action 2" />
+                    </CardActions>
+                </Card>
+            </div>
         )
     }
 }
